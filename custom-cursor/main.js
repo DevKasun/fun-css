61de144d35a829ad94a1758f8bb631ae93332d28
Kasun Lakshitha <kasun2014@outlook.com>
2024-09-18 23:49:59 +0530

    CSS custom cursor

---
+++ custom-cursor/main.js
@@ -0,0 +1,14 @@
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
+
+document.addEventListener('mousedown', () => {
+    cursor.classList.add('hover');
+});
+
+document.addEventListener('mouseup', () => {
+    cursor.classList.remove('hover');
+});
